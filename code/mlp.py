d8c04cf69e634a22e8335878be066b6b203ac27f
Vik Kamath <vikram.kmth@gmail.com>
2014-10-03 16:26:14 +0300

    some more incremental changes to mlp.py

--- code/mlp.py
+++ code/mlp.py
@@ -31,8 +31,38 @@ class HiddenLayer(object):
         :param 'input' : a symbolic tensor of shape (n_example, n_in)
         :type 'input' : theano.tensor.dmatrix
 
-        :param 'n_in':
-        :type 'n_in':
+        :param 'n_in': dimensionality of input
+        :type 'n_in': int
+
+        :param 'n_out': number of hidden units
+        :type 'n_out': int
+
+        :param 'activation' : Non linearity to be applied on the hidden layer
+        :type 'activation' : theano.Op or function
+        """
+
+        self.input = input
+
+        # 'W' is initalized randomly (uniformly) from the range 
+        #   sqrt(-5/[n_in + n_hidden]) to sqrt(+6/[n_in + n_hidden]) 
+        #   for the tanh activation function
+        # The output of rng.uniform is converted to an array and as floats
+        #       to allow the code to be run on a GPU (using asarray and 
+        #           dtype=theano.config.floatX respectively)
+        # Optimal initizliation weights are dependent on the non-linearlity used
+        #           refer Xavier, Bengio '10 for details. 
+        
+        if W is None: 
+            W_values = np.asarray(rng.uniform(
+                        low = -np.sqrt(6/ (n_in + n_out)),
+                        high = np.sqrt(6/ (n_in + n_out)),
+                        size = (n_in,n_out),
+                        dtype = theano.config.floatX))
+            if activation = theano.tensor.nnet.sigmoid:
+                W_values = W_values * 4 #Refer [Xavier,Bengio '10]
+            W = theano.shared(value = W_values,name = "W",borrow = True)
+        
+        if b is None: 
 
 
 
